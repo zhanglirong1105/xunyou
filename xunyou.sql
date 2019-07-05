64e57a6d2014259cc69d7f15f1a7b38ec7bd1ff8
maomao <971790437@qq.com>
2019-07-05 15:46:59 +0800

    sql

--- xunyou.sql
+++ xunyou.sql
@@ -1,7 +1,8 @@
 SET NAMES UTF8;
 DROP DATABASE IF EXISTS xunyou;
-CREATE DATABASE xunyou CHARSET=UTF8;
+CREATE DATABASE xunyou; CHARSET=UTF8;
 USE xunyou;
+
 CREATE TABLE xunyou_user(
  uid  INT,
  uname VARCHAR(18),
@@ -11,4 +12,21 @@ CREATE TABLE xunyou_user(
  phone VARCHAR(16),
 );
 
-CREATE TABLE xunyou_
+CREATE TABLE xunyou_product(
+  pid SMALLINT,
+  product_type BOOL,
+);
+
+CREATE TABLE xunyou_order(
+  oid INT PRIMARY KEY AUTO_INCREMENT,
+  order_id INT,           
+  product_id INT,        
+  count INT               
+);
+
+CREATE TABLE xunyou_action(
+   aid INT,
+   start time DATETIME,
+   end time DATETIME,
+   Event details VARCHAR(188)
+);
